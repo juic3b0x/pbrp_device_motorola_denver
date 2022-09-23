1191dc9cbfe2948b23ce84b44379f90bb2f968ad
ragarcia87 <ryn.anthony.garcia@gmail.com>
2022-09-23 02:56:36 -0500

    Initial Commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022-juic3b0x
+#
+
+PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/omni_denver.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_denver-eng
